1969f46ea654e109c2ea9597a1d0a6b7b31fa7d5
VVM16 <vvm00@iesemilidarder.com>
2025-04-01 12:17:34 +0200

    Add files via upload

---
+++ Ejercicio 1.2 JS/js/ejer-1.2.js
@@ -0,0 +1,60 @@
+let num1 = 4;
+let num2 = 6;
+let resultado;
+
+document.getElementById("result1").innerHTML="Hola Mundo"
+
+// SUMA
+
+resultado = num1 + num2
+console.log(resultado)
+console.log("La suma de " + num1+ " y " + num2 +" es " + resultado)
+
+// POTENCIA
+
+resultado = num1 ** num2
+console.log(resultado)
+console.log("La potencia de " + num1+ " elevado a " + num2 +" es " + resultado)
+
+// MULTIPLICACION
+
+resultado = num1 * num2
+console.log(resultado)
+console.log("La multiplicación de " + num1+ " por " + num2 +" es " + resultado)
+
+// DIVISION
+
+resultado = num1 / num2
+console.log(resultado)
+console.log("La división de " + num1+ " entre " + num2 +" es " + resultado)
+
+// MODULO
+
+resultado = num1 % num2
+console.log(resultado)
+console.log("El resto de la división de " + num1+ " entre " + num2 +" es " + resultado)
+
+// RESTA
+resultado = num1 - num2
+console.log(resultado)
+console.log("La resta de " + num1+ " menos " + num2 +" es " + resultado) 
+
+// INCREMENTO
+resultado = num1 ++; num2
+console.log(resultado)
+console.log("El incremento de " + num1+ " y " + num2 +" es " + resultado)
+
+// DECREMENTO
+resultado = num1 --;num2
+console.log(resultado)
+console.log("El decremento de " + num1+ " y " + num2 +" es " + resultado)
+
+// INCREMENTO DEFINIDO
+resultado = num1 += num2
+console.log(resultado)
+console.log("El incremento definido de " + num1+ " y " + num2 +" es " + resultado)
+
+// DECREMENTO DEFINIDO
+resultado = num1 -= num2
+console.log(resultado)
+console.log("El decremento definido de " + num1+ " y " + num2 +" es " + resultado)
